90b529aac7936c62adefccf94c2892041bc0b272
izzatasyraf <50535896+izzatasyraf@users.noreply.github.com>
2020-04-10 13:50:28 +0800

    debug

---
+++ myjs.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+        $('nav ul li a').css('color', 'white');
+    }
+
+    else {
+        $('nav').removeClass('black');
+        $('nav ul li a').css('color', '#4d4d4d')
+    }
+})
